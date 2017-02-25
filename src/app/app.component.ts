633cc64b78dca8c8e343d6b11108d0be63dd39aa
Oksana Senchuk <senchuk.oksana@gmail.com>
2017-02-25 15:46:34 +0200

    add login controller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<login></login>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+ }
